495f5f63697dbe4a0817731c956f8fccfd4143d7
Dmytro Redko <dmytro.redko@infopulse.com>
2024-06-11 14:02:14 +0300

    Refactor code a bit

--- godot/scenes/main.gd
+++ godot/scenes/main.gd
@@ -3,7 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$MainMenu.playInUi()
+	$MainMenu.newGame.connect(_on_new_game)
+	
+func _on_new_game():
+	$MainMenu.playOutUi()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- godot/scenes/main_menu.gd
+++ godot/scenes/main_menu.gd
@@ -1,15 +1,19 @@
 extends Control
 
+signal newGame()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$NewGameButton.pressed.connect(_on_new_game_button_pressed)
-	$UiAnimation.play_backwards("ui_animation")
 	
 func _on_new_game_button_pressed():
-	$UiAnimation.play("ui_animation")
-
+	self.newGame.emit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func playOutUi():
+	$UiAnimation.play("ui_animation")
+func playInUi():
+	$UiAnimation.play_backwards("ui_animation")
