f988ac3d35d8f08c34c7b3c5d260ca7db5ede28a
Dmytro Redko <dmytro.redko@infopulse.com>
2024-06-11 23:49:02 +0300

    Add random derangements

--- godot/scenes/main.gd
+++ godot/scenes/main.gd
@@ -1,12 +1,30 @@
 extends Node2D
 
-var game_screen_preload: Resource = preload("res://scenes/game_screen.tscn")
+const game_screen_preload: Resource = preload("res://scenes/game_screen.tscn")
+var _derangements_8: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MainMenu.playInUi()
 	$MainMenu.newGame.connect(_on_new_game)
+	_read_derangments()
+
+func _read_derangments():
+	const file_name: String = "res://derangements/8.json"
+	assert(FileAccess.file_exists(file_name))
+	var file_data = FileAccess.open(file_name, FileAccess.READ)
+	var parse_result = JSON.parse_string(file_data.get_as_text())
+	assert(parse_result is Array)
+	assert(parse_result.size() > 0)
+	self._derangements_8 = parse_result
 	
+func _get_random_derangement() -> Array:
+	assert(self._derangements_8.size() > 0)
+	var random_index = randi() % self._derangements_8.size()
+	var random_array: Array = self._derangements_8[random_index]
+	assert(random_array.size() == 8)
+	return random_array
+
 func _on_new_game():
 	if $MainMenu.playout_finished.is_connected(_on_new_game_playout_finished):
 		return
@@ -16,7 +34,7 @@ func _on_new_game():
 func _on_new_game_playout_finished():
 	print("_on_new_game_playout_finished")
 	var game_screen: GameScreen = game_screen_preload.instantiate() as GameScreen
-	game_screen.reset([7, 6, 5, 4, 3, 2, 1, 0])
+	game_screen.reset(self._get_random_derangement())
 	if game_screen:
 		get_parent().add_child(game_screen) # TODO: figure out why get_parent is needed
 		
