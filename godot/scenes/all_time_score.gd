4a4e66187b4b9cfd80b5f8bb57a4c3c8e7ce5f9a
Dmytro Redko <dmytro.redko@infopulse.com>
2024-06-12 09:58:48 +0300

    Make AllTimeScore scene a tool

--- godot/scenes/all_time_score.gd
+++ godot/scenes/all_time_score.gd
@@ -1,6 +1,12 @@
+@tool
 extends VBoxContainer
-
-@export var Title: String = "TITLE"
+@export var Title: String:
+	set(title):
+		Title = title
+		if $TitleLabel:
+			$TitleLabel.text = title
+	get:
+		return $TitleLabel.text
 # Called when the node enters the scene tree for the first time.
 func setScore(score:int):
 	$CenterContainer/ScoreLable.text = str(score)
